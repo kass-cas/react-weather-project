ebedc2e27ba84fbcae8a847d066ef3b9ee9d9b67
kass-cas <112037810+kass-cas@users.noreply.github.com>
2022-11-30 14:58:27 -0800

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <footer>
-        <a
-          href="https://github.com/kass-cas/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Kassandra Castillo
-      </footer>
+      <div className="container">
+        <h1>Hello World</h1>
+        <footer>
+          <a
+            href="https://github.com/kass-cas/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Kassandra Castillo
+        </footer>
+      </div>
     </div>
   );
 }
